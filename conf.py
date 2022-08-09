8405cf6d87887159fffe4155ab58f8789328f7a5
ErickYasser <103649685+ErickYasser@users.noreply.github.com>
2022-08-09 06:58:10 +0200

    Add files via upload

---
+++ conf.py
@@ -0,0 +1,5 @@
+api_hash = "0c780f6db3124fafbdc3dcde437e6f21"
+api_id = 16536294
+bot_token = "5217944853:AAFcr4oWWvrZL4AttLYccOVXSmv60aIoxc8"
+authorized_users = ['LAES2002']
+admin_user = 'LAES2002'
