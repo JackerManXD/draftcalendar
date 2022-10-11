328f323270aea84acb57108c57fa2d09b5ff2798
ErickYasser <103649685+ErickYasser@users.noreply.github.com>
2022-10-11 10:48:14 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,5 +1,5 @@
-api_hash = "9244d23f7072354594d2977064cc7b0c"
-api_id = 13380286
-bot_token = "5599851168:AAExhPzfv0hW2CgrIYzq6uHOqtwutalxdk8"
-authorized_users = ['HalconVip']
+api_hash = ""
+api_id = 13380234
+bot_token = ""
+authorized_users = ['']
 admin_user = 'ErickYasser'
